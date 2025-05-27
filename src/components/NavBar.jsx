2dab6dfe87cc0135a928cb144839849cc74ae85c
seongjaehong <tjdwo0423@gmail.com>
2025-05-27 19:33:20 +0900

    init navigation bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { CATEGORIES, NAME } from '../../literal';
 import { useLanguageContext } from '../contexts/LanguageContext';
 import './css/NavBar.css';
+import { faSquareGithub } from '@fortawesome/free-brands-svg-icons/faSquareGithub';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
 
 export default function NavBar() {
   const langManager = useLanguageContext();
-  langManager.lang = 'en';
-  console.log(langManager.lang);
   return (
     <aside className='navbar'>
       <div className='navbar__scrollbar' hidden={true}>
@@ -18,10 +19,26 @@ export default function NavBar() {
       </section>
       <section className='category'>
         <ul>
-          {CATEGORIES.map((category) => (
-            <li>{category[langManager.lang]}</li>
+          {CATEGORIES.map((category, index) => (
+            <li key={`category_${index}`}>{category[langManager.lang]}</li>
           ))}
         </ul>
+      </section>
+      <section className='external-link'>
+        <a
+          className='external-link__icon'
+          href='http://github.com/SeongjaeHong/'
+          target='_blank'
+        >
+          <FontAwesomeIcon icon={faSquareGithub} />
+        </a>
+        <a
+          className='external-link__icon'
+          href='https://www.linkedin.com/in/sj-hong/'
+          target='_blank'
+        >
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
       </section>
     </aside>
   );
