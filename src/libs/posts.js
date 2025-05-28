b607fe21f28c2b24727a22e91855255bcdcc3312
seongjaehong <tjdwo0423@gmail.com>
2025-05-29 01:46:55 +0900

    separate between title and contents

--- src/libs/posts.js
+++ src/libs/posts.js
@@ -5,9 +5,17 @@ const searchedPosts = import.meta.glob('../../posts/**/*.md', {
   import: 'default',
 });
 
+let title;
 export const posts = new Object();
 Object.entries(searchedPosts).map(([path, contents], index) => {
   const fileName = path.split('/').pop().replace('.md', '');
-  const title = contents.match(/^# (.+)/)?.[1] || fileName; // Use 1st # tag as a title
+  const match = contents.match(/^# (.+)\r?\n([\s\S]+)/m);
+
+  if (match) {
+    title = match[1];
+    contents = match[2];
+  } else {
+    title = fileName;
+  }
   Object.assign(posts, { [index]: { title, contents } });
 });
