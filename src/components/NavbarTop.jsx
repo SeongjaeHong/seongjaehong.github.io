6a4fbaae8fbcc90f5d87864466f7eac46ef8ff14
seongjaehong <tjdwo0423@gmail.com>
2025-05-28 16:45:41 +0900

    sidebar button animation

--- src/components/NavbarTop.jsx
+++ src/components/NavbarTop.jsx
@@ -1,10 +1,37 @@
 import './css/NavBarTop.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { NAME } from '../../literal';
+import { useState } from 'react';
 
 export default function NavBarTop() {
+  const [openSidebar, setOpenSidebar] = useState(false);
+  const openSidebarHandler = () => {
+    setOpenSidebar(!openSidebar);
+  };
   return (
-    <nav className='navbar-top'>
-      <div>Test div</div>
-      <p>Something P</p>
-    </nav>
+    <>
+      <nav className='navbar-top'>
+        <div className='gnb-left' onClick={openSidebarHandler}>
+          <button className={'fc-button ' + (openSidebar ? 'hide' : '')}>
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+        </div>
+        <div className='gnb-center'>
+          <a href='/'>{NAME}</a>
+        </div>
+      </nav>
+      <div className='sidebar-contents'>
+        <div className='sub-navigation'>
+          <button
+            id='close-sidebar'
+            className={'fc-button ' + (openSidebar ? 'show' : 'hide')}
+            onClick={openSidebarHandler}
+          >
+            <FontAwesomeIcon icon={faXmark} />
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
