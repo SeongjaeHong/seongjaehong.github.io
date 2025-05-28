3adaa4bd8fca682b717b7ed948809d1973b482d5
seongjaehong <tjdwo0423@gmail.com>
2025-05-29 02:19:24 +0900

    blur when scroll moved down

--- src/components/NavbarTop.jsx
+++ src/components/NavbarTop.jsx
@@ -3,12 +3,33 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { NAME } from '../../literal';
 import { useScreenContext } from '../contexts/ScreenContextProvider';
+import { useEffect, useState } from 'react';
 
 export default function NavBarTop({ openSidebar, openSidebarHandler }) {
   const mobile = useScreenContext();
+  const [scroll, setScroll] = useState(false);
+  const scrollHandler = () => {
+    const targetY = window.innerHeight * 0.1 + 180;
+    const curY = window.scrollY + 50;
+    if (curY >= targetY) {
+      setScroll(true);
+    } else {
+      setScroll(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', scrollHandler);
+    return () => {
+      window.removeEventListener('scroll', scrollHandler);
+    };
+  }, []);
 
   return (
-    <nav className={'navbar-top' + (mobile ? ' mobile' : '')}>
+    <nav
+      className={
+        'navbar-top' + (mobile ? ' mobile' : '') + (scroll ? ' scroll' : '')
+      }
+    >
       <div className='gnb-left' onClick={openSidebarHandler}>
         <button className={'fc-button' + (openSidebar ? ' hide' : '')}>
           <FontAwesomeIcon icon={faBars} />
--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -3,23 +3,30 @@ import remarkGfm from 'remark-gfm';
 import rehypeHighlight from 'rehype-highlight';
 import { posts } from '../libs/posts';
 import { useParams } from 'react-router';
+import './css/BlogPost.css';
 
 export default function BlogPost() {
   const { postId } = useParams();
+  const title = posts[postId]?.title;
   const content = posts[postId]?.contents;
 
   if (!content) {
-    return <div className='prose mx-auto p-4'>해당 글이 없습니다.</div>;
+    return <div className='mx-auto p-4'>해당 글이 없습니다.</div>;
   }
 
   return (
-    <div className='prose mx-auto p-4'>
-      <ReactMarkdown
-        remarkPlugins={[remarkGfm]}
-        rehypePlugins={[rehypeHighlight]}
-      >
-        {content}
-      </ReactMarkdown>
-    </div>
+    <>
+      <header className='contents-cover'>
+        <h1 className='headline'>{title}</h1>
+      </header>
+      <section className='contents'>
+        <ReactMarkdown
+          remarkPlugins={[remarkGfm]}
+          rehypePlugins={[rehypeHighlight]}
+        >
+          {content}
+        </ReactMarkdown>
+      </section>
+    </>
   );
 }
